d8e2759fd637eaa5b6409879baeb14a34b7f3ba2
Nak <58125177+Sovathanak@users.noreply.github.com>
2021-03-17 14:12:26 +1100

    Added print_board(board) function and player_choice() function
    - print_board(board): a function that will output the current state of the chessboard throughout the game as the players moves their pieces.
    
    - player_choice(): a function that asks the user for what color do they want to be when playing the chess game

--- chess.py
+++ chess.py
@@ -1,5 +1,48 @@
 def main():
-    return
+    player_choice()
+
+def player_choice():
+    player_choice = input("What color do you want to be? (w/b) ")
+    PLAYER_BLACK_BOARD = [["r", "n", "b", "q", "k", "b", "n", "r"],
+    ["p","p","p","p","p","p","p","p"],
+    ['.','.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.','.'],
+    ["P","P","P","P","P","P","P","P"],
+    ["R", "N", "B", "Q", "K", "B", "N", "R"]]
+    
+    PLAYER_WHITE_BOARD = [["r", "n", "b", "q", "k", "b", "n", "r"],
+    ["p","p","p","p","p","p","p","p"],
+    ['.','.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.','.'],
+    ["P","P","P","P","P","P","P","P"],
+    ["R", "N", "B", "Q", "K", "B", "N", "R"]]
+    if player_choice.lower() == "w":
+        return PLAYER_WHITE_BOARD
+    elif player_choice.lower() == "b":
+        return PLAYER_BLACK_BOARD
+    else:
+        print("Invalid choice, try again")
+        new_board()
+
+def print_board(board):
+    for i in range(len(board)):
+        row = ""
+        num = 0
+        for piece in board[i]:
+            if num < 8:
+                num +=1
+                row += piece + " "
+            else:
+                num += 1
+                row += piece + "\n"
+        
+        print(i+1, row)   # prints out the row labels and the board next to it
+    print("""  a b c d e f g h""")  # prints the columns of the board
+
 
 if __name__ == "__main__":
-    main()
+    main()b
