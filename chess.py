2f32cc443c9c6c86df1306720be37494356c874d
Nak <58125177+Sovathanak@users.noreply.github.com>
2021-03-19 01:20:41 +1100

    Updates to the chess.py
    - deleted clear_screen() function
    - added game_restart() function to restart the game
    - added additional features to other functions: valid_moves(), player_choice()

--- chess.py
+++ chess.py
@@ -5,39 +5,47 @@ class Chess:
         self.player = None
         self.playerchoice = None
         self.game_over = False
-        self.board = [["r", "n", "b", "q", "k", "b", "n", "r"],
-        ["p","p","p","p","p","p","p","p"],
-        ['.','.','.','.','.','.','.','.'],
-        ['.','.','.','.','.','.','.','.'],
-        ['.','.','.','.','.','.','.','.'],
-        ['.','.','.','.','.','.','.','.'],
+        self.board = [["R", "N", "B", "Q", "K", "B", "N", "R"],
         ["P","P","P","P","P","P","P","P"],
-        ["R", "N", "B", "Q", "K", "B", "N", "R"]]
+        ['.','.','.','.','.','.','.','.'],
+        ['.','.','.','.','.','.','.','.'],
+        ['.','.','.','.','.','.','.','.'],
+        ['.','.','.','.','.','.','.','.'],
+        ["p","p","p","p","p","p","p","p"],
+        ["r", "n", "b", "q", "k", "b", "n", "r"]]
         self.game_loop()
+
 
     def game_loop(self):
         self.player_setup()
-        self.clear_screen()
         self.print_board(self.board)
         while self.game_over != True:
+            print("========================================")
             self.player_choice()
-        print("Game Finished")
+            self.print_board(self.board)
+        print("==============Game Aborted==============")
+
+
+    def game_restart(self):
+        print("=============Game restarted=============\n")
+        self.__init__()
 
 
     def player_choice(self):
-        print("Player " + str(self.player)+" turn, what do you want to do? ")
+        print("Player " + str(self.player)+"'s turn, what do you want to do? ")
         self.playerchoice = input("Enter here: ")
-        if len(self.playerchoice) > 4:
-            print("Invalid input")
-            self.player_choice()
-        curr_pos, move_pos = self.playerchoice[:2], self.playerchoice[2:]
         if (self.playerchoice.lower() == "quit"):
             self.game_over = True
-        
-        
-
-    def clear_screen(self):
-        system("cls")
+        elif (self.playerchoice.lower() == "restart"):
+            self.game_restart()
+        elif (self.playerchoice.lower() == "check"):
+            check_piece_pos = input("Enter the piece's position on the current board state to check for valid moves: ")
+            print(self.valid_move(check_piece_pos, self.board))
+            self.player_choice()
+        else:
+            curr_pos, move_pos = (self.board[self.playerchoice[:2]][0], self.board[self.playerchoice[:2]][1]), (self.board[self.playerchoice[2:]][0], self.board[self.playerchoice[2:]][1])
+            if len(curr_pos) == 2 and len(move_pos) == 2:
+                return
 
 
     def swap_turn(self):
@@ -46,18 +54,26 @@ class Chess:
         else:
             self.player -= 1
 
+
     def player_setup(self):
-        player1_choice = input("Player1: What color do you want to be? (w/b) ")
+        player1_choice = input("Question for Player 1: What color do you want to be? (w/b) ")
         if player1_choice.lower() == "w":
             self.player = 1
+            print("Player " + str(self.player) +" is white")
+            print("Player " + str(self.player + 1) +" is black")
+            print("========================================")
         elif player1_choice.lower() == "b":
             self.player = 2
+            print("Player " + str(self.player - 1) +" is black")
+            print("Player " + str(self.player) +" is white")
+            print("========================================")
         else:
             print("Invalid choice, try again")
             self.player_setup()
 
 
-    def valid_move(self, curr_pos, move_pos):
+
+    def valid_move(self, curr_pos, board):
         return
 
 
@@ -68,12 +84,11 @@ class Chess:
             for piece in board[i]:
                 if num < 8:
                     num +=1
-                    row += piece + " "
+                    row += piece + "|"
                 else:
                     num += 1
                     row += piece + "\n"
-            print(i+1, row)   # prints out the row labels and the board next to it
-        print("""  a b c d e f g h""")  # prints the columns of the board
-
+            print(i+1, "|"+row)   # prints out the row labels and the board next to it
+        print("""   a b c d e f g h""")  # prints the columns of the board
 
 game = Chess()
